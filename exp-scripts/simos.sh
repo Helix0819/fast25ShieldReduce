567cbaaf45ac41a9309a5566feaba748c206a9cd
Helix0819 <834991203@qq.com>
2024-09-19 14:29:43 +0800

    first commit

---
+++ exp-scripts/simos.sh
@@ -0,0 +1,92 @@
+cd ./bin
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-1 /ramdisk/vm1-1
+./ShieldReduceClient -t o -i /ramdisk/vm1-1
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-2 /ramdisk/vm1-2
+./ShieldReduceClient -t o -i /ramdisk/vm1-2
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-3 /ramdisk/vm1-3
+./ShieldReduceClient -t o -i /ramdisk/vm1-3
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-4 /ramdisk/vm1-4
+./ShieldReduceClient -t o -i /ramdisk/vm1-4
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-5 /ramdisk/vm1-5
+./ShieldReduceClient -t o -i /ramdisk/vm1-5
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-6 /ramdisk/vm1-6
+./ShieldReduceClient -t o -i /ramdisk/vm1-6
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-7 /ramdisk/vm1-7
+./ShieldReduceClient -t o -i /ramdisk/vm1-7
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-8 /ramdisk/vm1-8
+./ShieldReduceClient -t o -i /ramdisk/vm1-8
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-9 /ramdisk/vm1-9
+./ShieldReduceClient -t o -i /ramdisk/vm1-9
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-10 /ramdisk/vm1-10
+./ShieldReduceClient -t o -i /ramdisk/vm1-10
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-11 /ramdisk/vm1-11
+./ShieldReduceClient -t o -i /ramdisk/vm1-11
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-12 /ramdisk/vm1-12
+./ShieldReduceClient -t o -i /ramdisk/vm1-12
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-13 /ramdisk/vm1-13
+./ShieldReduceClient -t o -i /ramdisk/vm1-13
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-14 /ramdisk/vm1-14
+./ShieldReduceClient -t o -i /ramdisk/vm1-14
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-15 /ramdisk/vm1-15
+./ShieldReduceClient -t o -i /ramdisk/vm1-15
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-16 /ramdisk/vm1-16
+./ShieldReduceClient -t o -i /ramdisk/vm1-16
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-17 /ramdisk/vm1-17
+./ShieldReduceClient -t o -i /ramdisk/vm1-17
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-18 /ramdisk/vm1-18
+./ShieldReduceClient -t o -i /ramdisk/vm1-18
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-19 /ramdisk/vm1-19
+./ShieldReduceClient -t o -i /ramdisk/vm1-19
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-20 /ramdisk/vm1-20
+./ShieldReduceClient -t o -i /ramdisk/vm1-20
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-21 /ramdisk/vm1-21
+./ShieldReduceClient -t o -i /ramdisk/vm1-21
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-22 /ramdisk/vm1-22
+./ShieldReduceClient -t o -i /ramdisk/vm1-22
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-23 /ramdisk/vm1-23
+./ShieldReduceClient -t o -i /ramdisk/vm1-23
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-24 /ramdisk/vm1-24
+./ShieldReduceClient -t o -i /ramdisk/vm1-24
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-25 /ramdisk/vm1-25
+./ShieldReduceClient -t o -i /ramdisk/vm1-25
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-26 /ramdisk/vm1-26
+./ShieldReduceClient -t o -i /ramdisk/vm1-26
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-27 /ramdisk/vm1-27
+./ShieldReduceClient -t o -i /ramdisk/vm1-27
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-28 /ramdisk/vm1-28
+./ShieldReduceClient -t o -i /ramdisk/vm1-28
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-29 /ramdisk/vm1-29
+./ShieldReduceClient -t o -i /ramdisk/vm1-29
+rm /ramdisk/*
+cp /mnt/image/multiuser_modifications/vm1-30 /ramdisk/vm1-30
+./ShieldReduceClient -t o -i /ramdisk/vm1-30
+rm /ramdisk/*
